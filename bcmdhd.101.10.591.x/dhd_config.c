95fde860e786b0470a10b6158c18055d995a5a1e
simonhust <96456374+simonhust@users.noreply.github.com>
2025-11-21 17:26:07 +0800

    Update dhd_config.c

--- bcmdhd.101.10.591.x/dhd_config.c
+++ bcmdhd.101.10.591.x/dhd_config.c
@@ -140,7 +140,7 @@ const chip_name_map_t chip_name_map[] = {
 	{BCM43751_CHIP_ID,	1,	DONT_CARE,	"bcm43751a1_pcie_ag",	""},
 	{BCM43751_CHIP_ID,	2,	DONT_CARE,	"bcm43751a2_pcie_ag",	""},
 	{BCM43752_CHIP_ID,	1,	DONT_CARE,	"bcm43752a1_pcie_ag",	""},
-	{BCM43752_CHIP_ID,	2,	DONT_CARE,	"bcm43752a2_pcie_ag",	""},
+	{BCM43752_CHIP_ID,	2,	DONT_CARE,	"bcm43752a2_pcie_ag",	"ap6275p"},
 	{BCM4375_CHIP_ID,	5,	DONT_CARE,	"bcm4375b4_pcie_ag",	"ap6275hh3"},
 #endif
 #ifdef BCMDBUS
@@ -169,6 +169,7 @@ const module_name_map_v2_t module_name_map_v2[] = {
 #ifdef BCMPCIE
 	{BCM4381_CHIP_ID,	0,	""},
 	{BCM4381_CHIP_ID,	1,	""},
+	{BCM43752_CHIP_ID,	2,	"ap6275p"},
 	{BCM43752_CHIP_ID,	4,	""},
 	{BCM43756_CHIP_ID,	4,	"ap6276p"},
 	{BCM43756_CHIP_ID,	6,	"ap6276p"},
@@ -219,6 +220,8 @@ const module_name_map_t module_name_map[] = {
 	 *  ModuleName		ChipName			Compat function
 	 */
 #ifdef BCMSDIO
+	{BCM43751_CHIP_ID,	BCM43752_CHIP_ID,	2,	0, 0,
+		"ap6275p",		"bcm43752a2_pcie_ag",	dhd_conf_compat_vht},
 	{BCM43751_CHIP_ID,	BCM43752_CHIP_ID,	2,	0, 0,
 		"ap6398s2",		"bcm4359c51a2_ag",	dhd_conf_compat_vht},
 	{BCM43751_CHIP_ID,	BCM43752_CHIP_ID,	2,	0, 0,
